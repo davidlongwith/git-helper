b93b4c7a363913f2c64dad475d5891b0e62dff26
David Longwith <david@davidlongwith.com>
2019-12-14 21:09:00 -0500

    rename some items for better code readability

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import "./App.css";
 
-function App({ commands }) {
+function App({ commandData }) {
   return (
     <div className="App">
       <header className="App-header">
         <p>App Header</p>
       </header>
-      <div className="Main">
-        {commands.map((commandGroup, i) => (
+      <div className="commands">
+        {commandData.map((commandGroup, i) => (
           <section
-            id={commandGroup.section.toLowerCase().replace(/ /g, "-")}
+            id={commandGroup.name.toLowerCase().replace(/ /g, "-")}
             className="command-group"
             key={i}
           >
-            <h1>{commandGroup.section}</h1>
+            <h1>{commandGroup.name}</h1>
             <div>
               {commandGroup.commands.map((command, i) => (
                 <div key={i}>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import data from "./data/commands";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App commands={data} />, document.getElementById("root"));
+ReactDOM.render(<App commandData={data} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
