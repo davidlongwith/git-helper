098dbd98af457feab5f763f58eac387e73811559
David Longwith <david@davidlongwith.com>
2019-12-23 19:24:19 -0500

    Updates

---
+++ precache-manifest.dc8aeafbe72cdf3fb7e7f371016bc2ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4491f79bf0688afb3843e6847cf85fe8",
+    "url": "/git-helper/index.html"
+  },
+  {
+    "revision": "ef9a2a18bb75cef50bad",
+    "url": "/git-helper/static/css/main.0521676a.chunk.css"
+  },
+  {
+    "revision": "60a8f9971302a573c349",
+    "url": "/git-helper/static/js/2.a7ea4794.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/git-helper/static/js/2.a7ea4794.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef9a2a18bb75cef50bad",
+    "url": "/git-helper/static/js/main.dcf0fbbc.chunk.js"
+  },
+  {
+    "revision": "9878bbfae971fbafa73d",
+    "url": "/git-helper/static/js/runtime-main.692549c8.js"
+  }
+]);
