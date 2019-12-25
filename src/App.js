a4597e7805471f07b135a48e86932764e1b359da
David Longwith <david@davidlongwith.com>
2019-12-24 20:16:19 -0500

    add comments for easier code readability

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import "./App.css";
 import CommandGroup from "./components/CommandGroup.js";
 
+// The "commandData" prop contains the data array from commands.json
 function App({ commandData }) {
   return (
     <div className="app">
       <header className="app-header">
         <h1>Git Helper</h1>
       </header>
+
+      <div className="jump-links"></div>
+
+      {/*
+        Iterate through each object in the commandData array for use in the
+        CommandGroup component.  For each iteration, the properties of that object
+        are added as props using the JSX spread operator.
+       */}
       <div className="commands">
         {commandData.map((commandGroup, i) => (
           <CommandGroup key={i} {...commandGroup} />
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import data from "./data/commands";
 import * as serviceWorker from "./serviceWorker";
 
+// App component imports commands.json data into prop named "commandData"
 ReactDOM.render(<App commandData={data} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
