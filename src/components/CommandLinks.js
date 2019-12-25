eae712c10bf16cc6ad16f94c7452d97c6cd3c8e0
David Longwith <david@davidlongwith.com>
2019-12-25 00:00:41 -0500

    functional command links

--- src/components/CommandLinks.js
+++ src/components/CommandLinks.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 //
-const CommandLinks = ({ name }) => <p>{name}</p>;
+const CommandLinks = ({ name }) => (
+  <div>
+    <a href={`#${name.toLowerCase().replace(/ /g, "-")}`}>{name}</a>
+  </div>
+);
 
 export default CommandLinks;
