af1d0971c479b731318ea8f346e15c0b7bdef7c8
David Longwith <david@davidlongwith.com>
2020-01-09 00:12:10 -0500

    create header component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import "./QuickLinks.css";
 import "./ScrollToTop.css";
+import Header from "./components/Header.js";
 import QuickLinks from "./components/QuickLinks.js";
 import Commands from "./components/Commands.js";
 import ScrollToTop from "./components/ScrollToTop.js";
@@ -11,9 +12,7 @@ import PropTypes from "prop-types";
 function App({ commandsByGroupArray }) {
   return (
     <div className="app">
-      <header className="app-header">
-        <h1>Git Helper</h1>
-      </header>
+      <Header />
       <QuickLinks sections={commandsByGroupArray} />
       <Commands commandData={commandsByGroupArray} />
       <ScrollToTop />
